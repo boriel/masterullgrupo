b5fa04dd28c5b30e741ee5646ba84ae67b09bda7
yormanh@gmail.com <yormanh@gmail.com@f2da8aa9-0175-0678-5dcd-d323193514b7>
2010-10-02 11:05:06 +0000

    En el Proyecto estaban todas estás dependencias que quité. Las dejo por aquí apuntadas: boton derecho, propiedades, vinculador, entrada winmm.lib;kernel32.lib;user32.lib;gdi32.lib;winspool.lib;comdlg32.lib;advapi32.lib;shell32.lib;ole32.lib;oleaut32.lib;uuid.lib;odbc32.lib;odbccp32.lib;%(AdditionalDependencies). Dejé solo 2.
    Realizado de los apuntes hasta la página 16. Antes del Init

---
+++ BasicEngine/BasicEngine/Window/Window.h
@@ -0,0 +1,39 @@
+/*
+Class Window.h
+
+	In this Window will run our game.
+
+Notes:
+	- Cuando se crea una aplicación en Windows, el sistema operativo crea un objeto que contiene la información asociada a la 
+		ejecución de nuestra aplicación. Podemos acceder a esa información a través de un manejador o handle del tipo HINSTANCE.
+	- Por otro lado, cuando se crea una ventana Windows, el sistema operativo almacena un objeto que representa a la ventana. 
+		Podemos acceder a ese objeto a través de un handledel tipo HWND.
+	- Por último, cuando tenemos la ventana y queremos “pintar” algo sobre ella, debemos acceder a su Contexto de Dispositivo 
+		o HDC. A través de este contexto podremosrenderizar OpenGL más tarde.
+
+*/
+
+
+#ifndef Window_H
+#define Window_H
+
+#include <Windows.h>
+
+
+class cWindow
+{
+	HINSTANCE mInstance;  // Holds the instance of the application
+	HWND mWnd;						// Holds our windows handle
+	HDC mDC;							// Private GDI Device context
+
+
+	//public functions
+	bool Init (cApplicationPropierties &lPropierties);
+	void Update();
+	bool DeInit();
+
+};
+
+
+
+#endif
