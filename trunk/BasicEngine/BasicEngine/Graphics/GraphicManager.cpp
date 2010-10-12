4f04b74956135dec1caee0936d7cdff54a1ee3ae
yormanh@gmail.com <yormanh@gmail.com@f2da8aa9-0175-0678-5dcd-d323193514b7>
2010-10-12 15:01:03 +0000

    Incluída la Librería matemáticas. Hecha la función de las primitivas principales. Pág 46.

--- trunk/BasicEngine/BasicEngine/Graphics/GraphicManager.cpp
+++ trunk/BasicEngine/BasicEngine/Graphics/GraphicManager.cpp
@@ -137,3 +137,97 @@ void cGraphicManager::SwapBuffer()
       SwapBuffers(mpWindow->GetHDC());
    }
 }
+
+
+
+//FUNCIONES DE RENDERIZADO AUXILIARES
+
+
+//Renderizado de un punto
+//Primer parámetro: posición donde se renderizará el punto.
+//Segundo parámetro: Color en RGB representado entre 0.0 y 1.0
+void cGraphicManager::DrawPoint (const cVec3 &lvPosition, const cVec3 &lvColor)
+{
+	glDisable(GL_TEXTURE_2D);
+	
+	// POINT
+	glPointSize(10);
+	glColor3f (lvColor.x, lvColor.y, lvColor.z);
+	glBegin(GL_POINTS);
+	glVertex3f(lvPosition.x, lvPosition.y, lvPosition.z);
+	glEnd ();
+	
+	glEnable(GL_TEXTURE_2D);
+}
+
+//Renderizado de una línea
+void cGraphicManager::DrawLine (const cVec3 &lvPosition1, const cVec3 &lvPosition2, const cVec3 &lvColor)
+{
+	glDisable(GL_TEXTURE_2D);
+	
+	// LINE
+	glLineWidth (1);
+	glColor3f (lvColor.x, lvColor.y, lvColor.z);
+	glBegin(GL_LINES);
+	for (float xtmp = -10.0; xtmp <= 10.0; xtmp += 1.0)
+	{
+		glVertex3f(lvPosition1.x, lvPosition1.y, lvPosition1.z);
+		glVertex3f(lvPosition2.x, lvPosition2.y, lvPosition2.z);
+	};
+	glEnd ();
+
+	glEnable(GL_TEXTURE_2D);
+}
+
+//Renderizado de una malla o rejilla
+void cGraphicManager::DrawGrid()
+{
+	glDisable(GL_TEXTURE_2D);
+
+	// GRID
+	glLineWidth (1);
+	glColor3f (1.0f, 1.0f, 1.0f);
+	glBegin(GL_LINES);
+	for (float lfxtmp = -10.0; lfxtmp <= 10.0; lfxtmp += 1.0)
+	{
+		glVertex3f (lfxtmp, 0.0f,-10.0);
+		glVertex3f (lfxtmp, 0.0f, 10.0);
+		glVertex3f (-10.0, 0.0f, lfxtmp);
+		glVertex3f ( 10.0, 0.0f, lfxtmp);
+	};
+	glEnd ();
+
+	glEnable(GL_TEXTURE_2D);
+
+}
+
+
+//Renderizado de los ejes
+//Rojo=X, G=Y, B=Z
+void cGraphicManager::DrawAxis()
+{
+	glDisable(GL_TEXTURE_2D);
+
+	// AXIS
+	glLineWidth (2);
+	glBegin(GL_LINES);
+
+	glColor3f(1.0f, 0.0f, 0.0f); // X Axis
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(1.0f, 0.0f, 0.0f);
+
+	glColor3f(0.0f, 1.0f, 0.0f); // Y Axis
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(0.0f, 1.0f, 0.0f);
+
+	glColor3f(0.0f, 0.0f, 1.0f); // Z Axis
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(0.0f, 0.0f, 1.0f);
+
+	glEnd();
+
+	glEnable(GL_TEXTURE_2D);
+}
