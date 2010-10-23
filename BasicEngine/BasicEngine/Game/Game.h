533278ec838ccd4a220f2252eb8d5a612b0d3851
yormanh@gmail.com <yormanh@gmail.com@f2da8aa9-0175-0678-5dcd-d323193514b7>
2010-10-23 13:32:32 +0000

    Se elimino y se añadio de nuevo el InputConfiguration
    Le del release si lo tenía en cuenta, se solucionó con:
    en el vinculador-general-bibliotecas adicionales añadí tanto en debug como en release .\Input\ois\dll\
    En el vinculador-Entrada-dependencias adicionales le puse OIS.lib tanto en debug como en release (que no estaba todavía).
    
    Después no ejecuta porque hay que copiar a mano el OIS_d.lib al bin\debug\, directorio de ejecución del juego.
    Tambien el OIS.dll hay que copiarlo a mano en bin\release. Estaba intentado averiguar que tanto las dll como las lib las cogiera del otro proyecto o bien que se se copian automaticamente al bin\debug o bin\release si se compilan de nuevo.
    
    
    Estas advertencias solo salen en debug, en release no sale creo recordar.
    
    Advertencia    2    warning MSB8012: TargetName(OIS) no coincide con el valor de la propiedad OutputFile de Library (OIS_static). Esto puede hacer que el proyecto no se compile correctamente. Para solucionar este problema, asegúrese de que los valores de propiedad $(OutDir), $(TargetName) y $(TargetExt) coinciden con el valor especificado en %(Lib.OutputFile).    C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\Microsoft.CppBuild.targets    1153    6    OIS
    
    Advertencia    1    warning MSB8012: TargetPath(F:\Ficheros\Cursos\Máster en Videojuegos ULL\04 Aplicación Práctica Básica de Videojuegos\Práctica SVN Google (en grupo)\BasicEngine\BasicEngine\Input\ois\Win32\..\lib\OIS.lib) no coincide con el valor de la propiedad OutputFile de Library (F:\Ficheros\Cursos\Máster en Videojuegos ULL\04 Aplicación Práctica Básica de Videojuegos\Práctica SVN Google (en grupo)\BasicEngine\BasicEngine\Input\ois\lib\OIS_static.lib). Esto puede hacer que el proyecto no se compile correctamente. Para solucionar este problema, asegúrese de que los valores de propiedad $(OutDir), $(TargetName) y $(TargetExt) coinciden con el valor especificado en %(Lib.OutputFile).    C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\Microsoft.CppBuild.targets    1151    6    OIS
    
    
    
    
    
    
    Sigue habiendo un fallo en el game.cpp en la línea del update
    //mbFinish = mbFinish || cWindow::Get().GetCloseApplication()    || IsPressed(eIA_CloseApplication);
    
    la macro #define no la pilla la verdad, dice ACTIOn no está definida, y esta vez si vi que compila el fichero la sustituí por esta.
    mbFinish = mbFinish || cWindow::Get().GetCloseApplication()    || cInputManager::Get().GetAction( eIA_CloseApplication ).GetIsPressed();

--- BasicEngine/BasicEngine/Game/Game.h
+++ BasicEngine/BasicEngine/Game/Game.h
@@ -18,11 +18,11 @@ Clase Game. Representa nuestro juego e implementa una estructura o ciclo de vida
 #include "..\Window\ApplicationProperties.h"
 #include "..\Graphics\Camera.h"
 
-#include "InputConfiguration.h"
-#include "..\Input\InputManager.h"
+//#include "InputConfiguration.h"
+//#include "..\Input\InputManager.h"
 
 
-extern tActionMapping kaActionMapping[];
+//extern tActionMapping kaActionMapping[];
 
 
 //using namespace std;
