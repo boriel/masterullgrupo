cccea645e4753482f155bdc3ce081290ece8e335
boriel@gmail.com <boriel@gmail.com@f2da8aa9-0175-0678-5dcd-d323193514b7>
2010-11-24 22:21:26 +0000

    Se añaden las sobrecargas de CallLua para 0 y 3 parámetros.

--- trunk/BasicEngine/BasicEngine/Lua/LuaManager.h
+++ trunk/BasicEngine/BasicEngine/Lua/LuaManager.h
@@ -48,8 +48,21 @@ class cLuaManager : public cSingleton<cLuaManager>
 
 		void Pop( unsigned int luiNValues );
 
+		// Función con 0 parámetros y uno de retorno
+		template <class Z>
+		bool CallLua( const char* lacFuncName, Z* lpRet);
+
+		// Función con 1 parámetro y otro de retorno
+		template <class T, class Z>
+		bool CallLua( const char* lacFuncName, T lArg1, Z* lpRet);
+
+		// Función con 2 parámetros y uno de retorno
 		template <class T, class U, class Z>
 		bool CallLua( const char* lacFuncName, T lArg1, U lArg2, Z* lpRet);
+
+		// Función con 3 parámetros y uno de retorno
+		template <class T, class U, class V, class Z>
+		bool CallLua( const char* lacFuncName, T lArg1, U lArg2, V lArg3, Z* lpRet);
 
 		friend class cSingleton<cLuaManager>;
 		
@@ -65,6 +78,93 @@ class cLuaManager : public cSingleton<cLuaManager>
 
 };
 
+
+
+template <class Z>
+bool cLuaManager::CallLua( const char* lacFuncName, Z* lpRet)
+{
+	assert( mpLuaContext );
+	
+	//Introducimos en la pila el nombre de la funcion
+	//definida en Lua que queremos llamar
+	lua_getglobal( mpLuaContext, lacFuncName );
+	
+	//Chequeamos que la función esta definida en Lua
+	int lbExists = lua_isfunction( mpLuaContext, -1 );
+	
+	//Si no esta definida en Lua
+	if ( !lbExists )
+	{
+		//Sacamos el nombre de la función de la pila de Lua
+		Pop( 1 );
+		printf( "Error: la funcion %s no existe\n", lacFuncName );
+		return false;
+	}
+
+	//Si la función esta definida en Lua...
+	//La funcion devuelve un valor
+	if ( lpRet )
+	{
+		//Llamamos a la funcion
+		lua_call( mpLuaContext, 0, 1 );
+		//Obtenemos el resultado
+		GetRetValue( lpRet );
+		//Eliminamos el valor de retorno de la pila
+		Pop( 1 );
+	}
+	//La función no devuelve ningun valor
+	else
+	{
+		//Llamamos a la función
+		lua_call( mpLuaContext, 0, 0 );
+	}
+	return true;
+}
+
+
+template <class T, class Z>
+bool cLuaManager::CallLua( const char* lacFuncName, T lArg1, Z* lpRet)
+{
+	assert( mpLuaContext );
+	
+	//Introducimos en la pila el nombre de la funcion
+	//definida en Lua que queremos llamar
+	lua_getglobal( mpLuaContext, lacFuncName );
+	
+	//Chequeamos que la función esta definida en Lua
+	int lbExists = lua_isfunction( mpLuaContext, -1 );
+	
+	//Si no esta definida en Lua
+	if ( !lbExists )
+	{
+		//Sacamos el nombre de la función de la pila de Lua
+		Pop( 1 );
+		printf( "Error: la funcion %s no existe\n", lacFuncName );
+		return false;
+	}
+
+	//Si la función esta definida en Lua...
+	//Introducimos en la pila el primer argumento
+	PushArg( lArg1 );
+	
+	//La funcion devuelve un valor
+	if ( lpRet )
+	{
+		//Llamamos a la funcion
+		lua_call( mpLuaContext, 1, 1 );
+		//Obtenemos el resultado
+		GetRetValue( lpRet );
+		//Eliminamos el valor de retorno de la pila
+		Pop( 1 );
+	}
+	//La función no devuelve ningun valor
+	else
+	{
+		//Llamamos a la función
+		lua_call( mpLuaContext, 1, 0 );
+	}
+	return true;
+}
 
 
 template <class T, class U, class Z>
@@ -114,6 +214,57 @@ bool cLuaManager::CallLua( const char* lacFuncName, T lArg1, U lArg2, Z* lpRet)
 	return true;
 }
 
+
+template <class T, class U, class V, class Z>
+bool cLuaManager::CallLua( const char* lacFuncName, T lArg1, U lArg2, V lArg3, Z* lpRet)
+{
+	assert( mpLuaContext );
+	
+	//Introducimos en la pila el nombre de la funcion
+	//definida en Lua que queremos llamar
+	lua_getglobal( mpLuaContext, lacFuncName );
+	
+	//Chequeamos que la función esta definida en Lua
+	int lbExists = lua_isfunction( mpLuaContext, -1 );
+	
+	//Si no esta definida en Lua
+	if ( !lbExists )
+	{
+		//Sacamos el nombre de la función de la pila de Lua
+		Pop( 1 );
+		printf( "Error: la funcion %s no existe\n", lacFuncName );
+		return false;
+	}
+
+	//Si la función esta definida en Lua...
+	//Introducimos en la pila el primer argumento
+	PushArg( lArg1 );
+	
+	//Introducimos en la pila el segundo argumento
+	PushArg( lArg2 );
+
+	//Introducimos en la pila el tercer argumento
+	PushArg( lArg3 );
+
+	
+	//La funcion devuelve un valor
+	if ( lpRet )
+	{
+		//Llamamos a la funcion
+		lua_call( mpLuaContext, 3, 1 );
+		//Obtenemos el resultado
+		GetRetValue( lpRet );
+		//Eliminamos el valor de retorno de la pila
+		Pop( 1 );
+	}
+	//La función no devuelve ningun valor
+	else
+	{
+		//Llamamos a la función
+		lua_call( mpLuaContext, 3, 0 );
+	}
+	return true;
+}
 
 
 
