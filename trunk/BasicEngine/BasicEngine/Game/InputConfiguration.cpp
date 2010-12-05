9795d0a17fe7aa3d5ac84cfe038108430702c153
boriel@gmail.com <boriel@gmail.com@f2da8aa9-0175-0678-5dcd-d323193514b7>
2010-12-05 16:59:43 +0000

    PRACTICA 6: Patrullero y personaje jugador, independientes. Se mueve con las teclas cursoras.

--- trunk/BasicEngine/BasicEngine/Game/InputConfiguration.cpp
+++ trunk/BasicEngine/BasicEngine/Game/InputConfiguration.cpp
@@ -5,6 +5,9 @@
 tActionMapping kaActionMapping[] = {
 	{ eIA_CloseApplication, eKeyboard, OIS::KC_ESCAPE },
 	{ eIA_CloseApplication, eMouse, eMouse_Button1}, // Se sale con el botón izquierdo
+	{ eIA_TurnLeft, eKeyboard, OIS::KC_LEFT },
+	{ eIA_TurnRight, eKeyboard, OIS::KC_RIGHT },
+	{ eIA_Advance, eKeyboard, OIS::KC_UP },
 	{ -1, -1, -1 } // End of the table (Marca el final de la tabla)
 	//{eIA_Up, eKeyboard, VK_UP },
 	//{eIA_Down, eKeyboard, VK_DOWN },
