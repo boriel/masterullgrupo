b263392736ca79e9f32e478472d3494c5cd574ec
boriel@gmail.com <boriel@gmail.com@f2da8aa9-0175-0678-5dcd-d323193514b7>
2011-01-20 23:14:39 +0000

    Siguiente paso. Carga la malla desde linea de comandos.

--- trunk/BasicEngine/meshbin/meshbin.cpp
+++ trunk/BasicEngine/meshbin/meshbin.cpp
@@ -1,9 +1,35 @@
 #include <stdlib.h>
+#include <stdio.h>
 
-#include "../BasicEngine/Utilities/ResourceManager.h"
+#include "../BasicEngine/Graphics/Meshes/MeshManager.h"
+#include "../BasicEngine/Graphics/Meshes/Mesh.h"
+
+void usage(void)
+{
+	puts("Usage:");
+	puts("meshbin <fich_in> <fich_out>");
+
+	return;
+	exit(1);
+}
+
 
 int main(int argc, char *argv[])
 {
+	cResourceHandle lScene;
+	cMesh *lpMesh;
+
+	if (argc != 3)
+		usage();
+
+	cMeshManager::Get().Init(10);
+	lScene = cMeshManager::Get().LoadResource("dragon", "./dragonsmall.DAE");
+	lpMesh = (cMesh *)lScene.GetResource();
+
+	printf("%i %i\n", lpMesh->getVertices(), lpMesh->getIndex());
+
+	//mScene = cSceneManager::Get().LoadResource("TestLevel", "./Data/Scene/dragonsmall.DAE");
+
 
 	return EXIT_SUCCESS;
 }
