a42f8e9a277cdf1fb8fa07992dd52511c19c3b03
yormanh <yormanh@f2da8aa9-0175-0678-5dcd-d323193514b7>
2011-05-06 13:24:16 +0000

    Copiado casi todo el Skeletal, pero taodvía no funciona, faltan cosas y arreglar otras. Comentado todos los ficheros

---
+++ trunk/BasicEngine/BasicEngine/Graphics/Skeletal/SkeletalManager.h
@@ -0,0 +1,48 @@
+/*
+class cSkeletalMesh: 
+
+
+Cal3D funciona siguiendo un modelo “core-instance”, esto es que entre
+distintos modelos animados de un mismo tipo existen unos datos que son comunes y
+otros que dependen de cada instancia.
+
+De esta forma, toda la información que es común se pone en una clase llamada “core” y
+toda la información que depende de la instancia se pone en una clase llamada
+“instance”. De esta forma se evita duplicar datos en memoria innecesariamente.
+
+esta clase tiene que gestionar dos tipos de resources, por
+lo que usaremos el sistema que ya tiene implementado el ResourceManager para
+gestionar los cores y añadimos una lista de SkeletalMesh (definiremos esta clase en las
+siguientes secciones) que representa la instancia del modelo esqueletal. Para crear las
+instancias hemos añadido la función CreateSkeletalMesh, que recibe el nombre del core
+que debe usarse para crear la instancia.
+
+*/
+
+/*
+#ifndef SKELETAL_MESH
+#define SKELETAL_MESH
+
+#include "..\..\Utility\ResourceManager.h"
+#include "..\..\Utility\Singleton.h"
+
+class cSkeletalMesh;
+
+class cSkeletalManager : public cResourceManager, public cSingleton<cSkeletalManager>
+{
+	public:
+		virtual void Init( unsigned luiMaxSize );
+		friend class cSingleton<cSkeletalManager>;
+		cSkeletalMesh * CreateSkeletalMesh( const std::string& lacCoreModelName );
+
+	protected:
+		cSkeletalManager() { ; } // Protected constructor
+
+	private:
+		virtual cResource * LoadResourceInternal( std::string lacNameID, const std::string &lacFile );
+};
+
+
+
+#endif
+*/
---
+++ trunk/BasicEngine/BasicEngine/Graphics/Skeletal/SkeletalMesh.cpp
@@ -0,0 +1,108 @@
+/*
+#include "SkeletalMesh.h"
+#include "../GLHeaders.h"
+#include <assimp.hpp> // C++ importer interface
+#include <aiScene.h> // Output data structure
+#include <aiPostProcess.h> // Post processing flags
+#include <cassert>
+#include "SkeletalCoreModel.h"
+#include "../GraphicManager.h"
+#include "../../MathLib/MathLib.h"
+
+
+bool cSkeletalMesh::Init( const std::string &lacNameID, void * lpMemoryData, int liDataType )
+{
+	cSkeletalCoreModel * lpCoreModel =
+	(cSkeletalCoreModel *)lpMemoryData;
+	lpCoreModel->CreateInstance(this);
+	return true;
+}
+
+void cSkeletalMesh::Deinit()
+{
+	delete mpCal3DModel;
+}
+
+void cSkeletalMesh::Update(float lfTimestep)
+{
+	mpCal3DModel->update(lfTimestep);
+}
+
+bool cSkeletalMesh::PlayAnim(const std::string & lacAnimName, float lfWeight, float lfDelayIn, float lfDelayOut)
+{
+	// Search for the animation
+	sAnimationDef* lpDef = mpCoreModel->GetAnimationDef(lacAnimName);
+	if (lpDef)
+	{
+		if (lpDef->meAnimType == eAnimType_Cycle)
+		{
+			// Blend the cycle
+			mpCal3DModel->getMixer()->blendCycle( lpDef->miAnimID,
+			lfWeight, lfDelayIn );
+		}
+		else
+		{
+			// It´s an action
+			mpCal3DModel->getMixer()->executeAction( lpDef->miAnimID,
+			lfDelayIn, lfDelayOut, lfWeight );
+		}
+
+		return true;
+	}
+
+	return false;
+}
+	
+
+void cSkeletalMesh::StopAnim(const std::string & lacAnimName,	float lfDelayOut)
+{
+	sAnimationDef* lpDef= mpCoreModel->GetAnimationDef(lacAnimName);
+	if (lpDef)
+	{
+		if (lpDef->meAnimType == eAnimType_Cycle)
+		{
+			// Blend the cycle
+			mpCal3DModel->getMixer()->clearCycle(lpDef->miAnimID,
+			lfDelayOut);
+		}
+		else
+		{
+			// It´s an action
+			mpCal3DModel->getMixer()->removeAction( lpDef->miAnimID,
+			lfDelayOut);
+		}
+	}
+}
+
+//se resetea la matriz de mundo y se le solicita a Cal3D que de una
+//lista de líneas (representadas como X1, Y1, Z1, X2, Y2, Z2). Además, establece un límite
+//de 120 bones en el modelo (esto se podía cambiar a un array dinámico si es necesario).
+//Una vez se tienen las líneas que representan los bones, se renderiza la línea. Lo único
+//que es un poco más raro es la escala que se aplica a la matriz de mundo, pero esto es
+//porque el modelo que estamos empleando es muy grande, por lo que lo escalamos a
+//0.01. Cuando se empleen modelos con la escala correcta, se puede eliminar este
+//escalado.
+void cSkeletalMesh::RenderSkeleton(void)
+{
+	cMatrix lWorld;
+	lWorld.LoadIdentity();
+	lWorld.LoadScale(0.01f);
+	cGraphicManager::Get().SetWorldMatrix(lWorld);
+	unsigned luiBoneCount = mpCal3DModel->getSkeleton()->getCoreSkeleton()->getVectorCoreBone().size();
+	assert(luiBoneCount < 120);
+	float mafLines[120] [6];
+	int liBones = mpCal3DModel->getSkeleton()->getBoneLines(&mafLines[0][0]);
+	
+	for (int liIndex = 0; liIndex < liBones; ++liIndex)
+	{
+		cVec3 lvP1(mafLines[liIndex][0], mafLines[liIndex][1],mafLines[liIndex][2]);
+		cVec3 lvP2(mafLines[liIndex][3], mafLines[liIndex][4],mafLines[liIndex][5]);
+		cGraphicManager::Get().DrawLine( lvP1, lvP2, cVec3(1.0f, 1.0f, 1.0f));
+	}
+
+	lWorld.LoadIdentity();
+	cGraphicManager::Get().SetWorldMatrix(lWorld);
+}
+*/
+
+
