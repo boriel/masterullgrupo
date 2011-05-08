ca36793febf22530c27b53260d1e1033f089cfd5
yormanh@gmail.com <yormanh@gmail.com@f2da8aa9-0175-0678-5dcd-d323193514b7>
2011-05-08 09:38:36 +0000

    Configurada las telcas para la animación (Q,W,E,R). Pág 71 hecha. Falta XML de shaders y malla.

--- BasicEngine/BasicEngine/Game/InputConfiguration.cpp
+++ BasicEngine/BasicEngine/Game/InputConfiguration.cpp
@@ -8,6 +8,11 @@ tActionMapping kaActionMapping[] = {
 	{ eIA_TurnLeft, eKeyboard, OIS::KC_LEFT },
 	{ eIA_TurnRight, eKeyboard, OIS::KC_RIGHT },
 	{ eIA_Advance, eKeyboard, OIS::KC_UP },
+	{ eIA_PlayJog, eKeyboard, OIS::KC_Q },
+	{ eIA_StopJog, eKeyboard, OIS::KC_W },
+	{ eIA_PlayWave, eKeyboard, OIS::KC_E },
+	{ eIA_StopWave, eKeyboard, OIS::KC_R },
+
 	{ -1, -1, -1 } // End of the table (Marca el final de la tabla)
 	//{eIA_Up, eKeyboard, VK_UP },
 	//{eIA_Down, eKeyboard, VK_DOWN },
