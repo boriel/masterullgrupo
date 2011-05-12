0b73b9e64b33ed059396575f2158602959091505
davidvargas.tenerife <davidvargas.tenerife@f2da8aa9-0175-0678-5dcd-d323193514b7>
2011-05-12 17:20:41 +0000

    Cambios menores en main de BulletAppTest.

--- trunk/BasicEngine/BulletAppTest/src/main.cpp
+++ trunk/BasicEngine/BulletAppTest/src/main.cpp
@@ -1,72 +1,47 @@
 /*
 Test para probar la librería Bullet
-
 */
 
+//DEMO_1: HelloWorld
+//DEMO_2: Vehicle
+#define DEMO_2
 
 //Demo del Vehicle
 #include "VehicleDemo.h"
 #include "GlutStuff.h"
 #include "GLDebugDrawer.h"
 #include "btBulletDynamicsCommon.h"
-GLDebugDrawer	gDebugDrawer;
 
-
-
+#ifdef DEMO_1
 //Demo del helloWorld
 #include "HelloWorld.h"
 
-//Cabeceras para encontrar las funciones de abajo
-int DemoVehicle (int argc, char** argv);
-int DemoHelloWorld();
-
-
-//Main Principal
-int main(int argc, char** argv)
-{
-
-	//Para ir probando cosas o demas demos en este espaci, comentamos y hacemos la llamada a la funcion
-
-	DemoVehicle (argc, argv);
-	//DemoHelloWorld();
-
-
-}
-
-
-
-
-
-
-
-
-int DemoVehicle (int argc, char** argv)
-{
-
-	VehicleDemo* vehicleDemo = new VehicleDemo;
-
-	vehicleDemo->initPhysics();
-	vehicleDemo->getDynamicsWorld()->setDebugDrawer(&gDebugDrawer);
-
-	return glutmain(argc, argv, 640, 480, "Bullet Vehicle Demo. http://www.continuousphysics.com/Bullet/phpBB2/", vehicleDemo);
-
-
-}
-
-
-
-
-
-int DemoHelloWorld()
-{
-
+int DemoHelloWorld() {
 	HelloWorld();
-
 	return 0;
 }
+#else
+//Demo Vehicle
+GLDebugDrawer	gDebugDrawer;
 
+int DemoVehicle (int argc, char** argv) {
+	VehicleDemo* plVehicleDemo = new VehicleDemo;
 
+	plVehicleDemo->initPhysics();
+	plVehicleDemo->getDynamicsWorld()->setDebugDrawer(&gDebugDrawer);
 
+	return glutmain(argc, argv, 640, 480, "Bullet Vehicle Demo. http://www.continuousphysics.com/Bullet/phpBB2/", plVehicleDemo);
+}
+#endif
 
-
+//===============================
+//Main Principal
+int main(int argc, char** argv) {
+	//Para ir probando cosas o demas demos en este espaci, comentamos y hacemos la llamada a la funcion
+#ifdef DEMO_1
+	DemoHelloWorld();
+#else
+	DemoVehicle (argc, argv);
+#endif
+}
 
