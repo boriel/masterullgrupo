3426b5bbdad6369b80ab469dd540db7edd31a065
yormanh@gmail.com <yormanh@gmail.com@f2da8aa9-0175-0678-5dcd-d323193514b7>
2011-05-24 20:45:44 +0000

    (nada importante)

--- trunk/BasicEngine/BasicEngine/Game/Object/ObjectPlayer.cpp
+++ trunk/BasicEngine/BasicEngine/Game/Object/ObjectPlayer.cpp
@@ -22,63 +22,14 @@ void cObjectPlayer::Update( float lfTimestep )
 
 	cObject::Update(lfTimestep); //Llmando al padre que tiene las fisicas generales
 
-	//PRUEBA1
-	//Estas dos lineas van bien, o eso parece
 	//mPosition = ((cPhysicsPlayer*)mPhysicsObject)->GetPosition();
-	//mWorldMatrix.SetPosition(mPosition);
-	//mWorldMatrix.LoadTranslation(mPosition);  //creo que es lo mismo esto que el setposition
-
-	////PRUEBA2 CASI FUNCIONA
-	////Primero rotamos la matriz
-	//cQuaternion lQuatRot = ((cPhysicsPlayer*)mPhysicsObject)->GetQuatRotation();
-	//mWorldMatrix.LoadRotation(cVec3(lQuatRot.x, lQuatRot.y, lQuatRot.z), lQuatRot.w);
-	////Depsues trasladamos la matriz
-	//mPosition = ((cPhysicsPlayer*)mPhysicsObject)->GetPosition();
-	//mWorldMatrix.SetPosition(mPosition);
-
-
-	//cQuaternion lQuatRot = ((cPhysicsPlayer*)mPhysicsObject)->GetQuatRotation();
-	//mWorldMatrix.LoadRotation(cVec3(lQuatRot.x, lQuatRot.y, lQuatRot.z), lQuatRot.w);
-
-	//mPosition = ((cPhysicsPlayer*)mPhysicsObject)->GetPosition();
-	//mWorldMatrix.SetPosition(mPosition);
-
-
-	mPosition = ((cPhysicsPlayer*)mPhysicsObject)->GetPosition();
-	cQuaternion lQuatRot=((cPhysicsPlayer*)mPhysicsObject)->GetQuatRotation();
+	mPosition = mPhysicsObject->GetPosition();
+	//cQuaternion lQuatRot=((cPhysicsPlayer*)mPhysicsObject)->GetQuatRotation();
+	cQuaternion lQuatRot= mPhysicsObject->GetQuatRotation();
 	lQuatRot.AsMatrix(mWorldMatrix);
 	mWorldMatrix.SetPosition(mPosition);
 
 
-
-	//((cPhysicsPlayer*)mPhysicsObject)->Pruebas2();
-
-	
-	//mPosition = ((cPhysicsPlayer*)mPhysicsObject)->GetPosition();
-	//mWorldMatrix.SetPosition(cVec3(mPosition.y, mPosition.z, mPosition.x));
-	
-
-
-
-
-	//((cPhysicsPlayer*)mPhysicsObject)->Pruebas();
-
-	
-
-  //cGraphicManager::Get().DrawAxis();
-
-/*
-	cQuaternion lQuatRotation = ((cPhysicsPlayer*)mPhysicsObject)->GetQuatRotation();
-	cVec3 lVec3 (lQuatRotation.x, lQuatRotation.y, lQuatRotation.z);
-	mWorldMatrix.LoadRotation(lVec3, lQuatRotation.w);
-	
-	mPosition = ((cPhysicsPlayer*)mPhysicsObject)->GetPosition();
-	mWorldMatrix.SetPosition(mPosition);
-*/
-
-	//mPosition = mPosition + cVec3(-1.0, 0, 0);
-	////Probando posiciones
-	//mWorldMatrix.SetPosition(mPosition);
 
 	
 }
@@ -91,8 +42,6 @@ void cObjectPlayer::Render ()
 	cObject::Render();
 
 #ifdef _DEBUG
-	//mPhysicsObject->RenderTransformDebug(((cPhysicsPlayer *)mPhysicsObject)->GetTransform(), 1.0);
-	//mPhysicsObject->RenderObjectDebug(((cPhysicsPlayer *)mPhysicsObject)->GetTransform(), 1.0);
 	((cPhysicsPlayer*) mPhysicsObject)->RenderObjectDebug();
 #endif
 	//cPhysicsObject::DrawTransform(lbtTransform, 1.0);
