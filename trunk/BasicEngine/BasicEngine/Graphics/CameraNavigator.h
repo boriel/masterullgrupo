da4a0e6277a5d3f14868f6a62c4a55beeda9e805
davidvargas.tenerife <davidvargas.tenerife@f2da8aa9-0175-0678-5dcd-d323193514b7>
2011-05-31 16:52:46 +0000

    .

---
+++ trunk/BasicEngine/BasicEngine/Graphics/CameraNavigator.h
@@ -0,0 +1,22 @@
+/*
+	Clase Cámara con navegación por el mundo
+*/
+
+#ifndef _CAMERA_NAVIGATOR_H
+#define _CAMERA_NAVIGATOR_H
+
+#include "Camera.h"
+
+class cCameraNavigator : public cCamera {
+	private:
+		cVec3 *mpPosition;
+		cVec3 *mpTarget;
+		float mfDespX;
+
+	public:
+		void Init(void);
+		void Deinit(void);
+		void Update(void);
+};
+
+#endif
